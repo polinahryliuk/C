b852db743657d379d7b61956fc61f17a8a956d6c
polinahryliuk <157729049+polinahryliuk@users.noreply.github.com>
2024-10-06 21:41:35 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿namespace Q9;
+class Program
+{
+    static void Main(string[] args)
+    {
+        decimal totalSales = 0;
+        decimal sales = 0;
+        int storeCount = 0;
+        string message = " ";
+
+        string path = @"../../../sales.txt";
+
+        string[] fileContents = File.ReadAllLines(path);
+
+        Console.WriteLine("Sales Report");
+        Console.WriteLine("Store ID\tSales\t\t Performance");
+   
+        foreach(string line in fileContents)
+        {
+            string[] data = line.Split(',');
+
+            string storeID = data[0];
+            sales = decimal.Parse(data[1]);
+
+            if (sales < 1000)
+            {
+                message = "Needs attention";
+            }
+            else if (sales > 1001 && sales < 1999)
+            {
+                message = "Moderate";
+            }
+            else if (sales > 2000)
+            {
+                message = "Very good";
+            }
+
+            Console.WriteLine($"\n{storeID}\t\t{sales}\t\t{message}");
+
+            totalSales += sales;
+            storeCount++;
+        }
+
+        Console.WriteLine($"\nTotal Sales : {totalSales}");
+        Console.WriteLine($"Average Sales : {totalSales/storeCount}");
+    }
+}
+
